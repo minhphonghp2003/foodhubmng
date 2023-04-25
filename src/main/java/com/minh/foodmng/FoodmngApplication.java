1f78d05af425882402ea9e90d50d1f17fad6ad21
minhphonghp2003 <minhphonghp2003@gmail.com>
2023-04-25 12:07:50 +0700

    init

---
+++ src/main/java/com/minh/foodmng/FoodmngApplication.java
@@ -0,0 +1,13 @@
+package com.minh.foodmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minh/foodmng/FoodmngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.foodmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
