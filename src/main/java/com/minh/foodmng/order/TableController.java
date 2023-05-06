c673566ec6493fc82c2dce494f52624d4895f366
minhphonghp2003 <minhphonghp2003@gmail.com>
2023-05-06 16:17:57 +0700

    table

--- src/main/java/com/minh/foodmng/order/TableController.java
+++ src/main/java/com/minh/foodmng/order/TableController.java
@@ -1,5 +1,7 @@
 package com.minh.foodmng.order;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TableController {
     final private TableService tableService;
+
     @GetMapping("/all")
-    public ResponseEntity<Page<Table>> getAllTable(int page, int size){
-        Page<Table> tables = tableService.findAll(page,size);
+    public ResponseEntity<Page<Table>> getAllTable(int page, int size) {
+        Page<Table> tables = tableService.findAll(page, size);
         return ResponseEntity.ok(tables);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Table> getTableBy(@PathVariable  Integer id){
+    public ResponseEntity<Table> getTableBy(@PathVariable Integer id) {
         Table table = tableService.findById(id);
         return ResponseEntity.ok(table);
     }
